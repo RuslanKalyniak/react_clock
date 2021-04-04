70cd7ead5651710077974f86a7b1b31fffbb68d0
Ruslan Kalyniak <krv241085krv@icloud.com>
2021-04-04 16:48:29 +0300

    Updates

---
+++ precache-manifest.594873010d0efa50b3dca931e6dc011d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c22e1e5865525b1e6380565da64dbb4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b836f51a1312b8e1beff",
+    "url": "./static/css/main.ecdb25d3.chunk.css"
+  },
+  {
+    "revision": "9bfabb54970d8a1edebf",
+    "url": "./static/js/2.c17dec48.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.c17dec48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b836f51a1312b8e1beff",
+    "url": "./static/js/main.ee850c7d.chunk.js"
+  },
+  {
+    "revision": "89de80f3985b25002b5f",
+    "url": "./static/js/runtime-main.17b9eccd.js"
+  },
+  {
+    "revision": "16f6975ae97dc6c494797891f4c01d48",
+    "url": "./static/media/1.16f6975a.jpeg"
+  },
+  {
+    "revision": "90992eaa8f4989c50be3f2cd381cfe4e",
+    "url": "./static/media/3.90992eaa.jpg"
+  }
+]);
